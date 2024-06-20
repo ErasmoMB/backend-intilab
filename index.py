0b51cd187718899e398b0d7275d543f1e9e803e2
ErasmoMB <montufarbe@gmail.com>
2024-06-20 12:11:27 -0500

    archivo prueba conectado a db atlas

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, render_template
 from flask_cors import CORS
 from src.routes.routes import bp as routes_bp  # Importa el Blueprint
 import os
@@ -6,14 +6,22 @@ import os
 # Obtén la ruta absoluta del directorio actual
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-app = Flask(__name__, template_folder=os.path.join(dir_path, './src/templates'), static_folder=os.path.join(dir_path, './src/static'))
+# Crea la aplicación Flask
+app = Flask(__name__,
+            template_folder=os.path.join(dir_path, './src/templates'),
+            static_folder=os.path.join(dir_path, './src/static'))
+
+# Habilita CORS
 CORS(app)
 
-app.register_blueprint(routes_bp)  # Registra el Blueprint
+# Registra el Blueprint
+app.register_blueprint(routes_bp)
 
+# Ruta para la página de inicio
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# Punto de entrada para ejecutar la aplicación
 if __name__ == '__main__':
     app.run(debug=True)
